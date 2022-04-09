d7b6688cff80ca5409c3e77c1a811f9b55577eaf
IvlKsu <ivl.ksyusha@mail.ru>
2022-04-09 19:07:58 +1000

    add pytest Homework5

---
+++ HW5/pytast_task01.py
@@ -0,0 +1,21 @@
+import pytest
+from HW5.HW_task01 import Homework, Teacher, Student
+
+expired_homework = Homework('calculate', 0)
+oop_homework = Homework('read', 5)
+student = Student('Roman', 'Petrov')
+teacher = Teacher('Daniil', 'Ivanov')
+
+
+def test_is_active():
+    assert expired_homework.is_active() == False
+    assert oop_homework.is_active() == True
+
+
+def test_create_homework():
+    assert teacher.create_homework("do something", 5).text == 'do something'
+
+
+def test_student_do_hw_method():
+    assert student.do_homework(expired_homework) is None
+    assert student.do_homework(oop_homework) == oop_homework
