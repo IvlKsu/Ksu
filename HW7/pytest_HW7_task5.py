76cc1ea40de18e5f27734ec5b86fbd796ed5fe55
IvlKsu <ivl.ksyusha@mail.ru>
2022-05-07 18:28:04 +1000

    add pytest Homework7

---
+++ HW7/pytest_HW7_task5.py
@@ -0,0 +1,14 @@
+from HW7.HW7_task5 import merge_sorted_files
+
+
+file1 = open("file1.txt", "w+")
+file1.writelines(["3\n", "8\n", "10\n"])
+file1.close()
+
+file2 = open("file2.txt", "w+")
+file2.writelines(["1\n", "2\n", "5\n"])
+file2.close()
+
+
+def test_task5():
+    assert merge_sorted_files(["file1.txt", "file2.txt"]) == [1, 2, 3, 5, 8, 10]
